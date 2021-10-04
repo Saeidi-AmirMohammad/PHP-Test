75b6f47cf862627bf40589d290866d42df78e0d7
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-10-04 23:55:48 +0330

    VariablesInPHP

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHPTest</title>
+    <title>Document</title>
 </head>
+
 <body>
-<?php
-    echo "Moghadampoor";
+    <?php
+    $fName = "AmirMohammad";
+    $lName = "Saeidi";
+    echo $fName, $lName;
+    ?>
+    <br>
+    <?php
+    $num1 = 10;
+    $num2 = 15;
+    echo $num1 + $num2;
     ?>
 </body>
+
 </html>
