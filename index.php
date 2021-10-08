fc14d6c0350cc1050fbfdeca1f9fd718dba0b6dd
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-10-08 23:53:23 +0330

    ForeachInPHP

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 
 <body>
     <?php
-    $list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    for($i = 0; $i < 10; $i++){
-        echo $list[$i];
-    }
+    $list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+   foreach($list as $item){
+       echo $item . " ";
+   }
 
 
     ?>
