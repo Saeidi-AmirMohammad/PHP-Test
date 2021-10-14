2118c180d71a18b294546c7c7c816ee74400b893
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-10-14 22:43:09 +0330

    $_GETInPHP

--- index.php
+++ index.php
@@ -10,41 +10,14 @@
 
 <body>
     <?php
-    // $list = [
-    //     'name' => 'amir',
-    //     'family' => 'saeidi'
-    // ];
-
-    // $list2 = [
-    //     'age' => 23,
-    //     'code' => 333
-    // ];
-
-    // echo count($list);
-    // echo is_array($list);
-    // echo in_array('amir' , $list);
-    // echo array_merge($list, $list2);
-
-    // echo array_key_exists('aa' , $list2);
-    // echo array_push($list2 , 'rightItem');
-    // echo array_shift($list2 , 'leftItem');
-    // echo array_pop($list2);
-    // echo array_flip($list);
-
-    $numa = [3, 5, 1, 7, 9, 0, 8, 4];
-
-    // echo  sort($numa);
-    // echo rsort($numa);
-
-    $list3 = [
-        'amir' => 20,
-        'reza' => 18,
-        'ahmad' => 12,
-        'kian' => 11
-    ];
-
-    //  echo ksort($list3);
-    // echo krsort($list3);
+if(isset($_GET['username']) && isset($_GET['pass'])){
+    $username = $_GET['username'];
+    $pass = $_GET['pass'];
+    var_dump($username . " " . $pass);
+}
+else{
+    echo "username or pass is not exsist";
+}
     ?>
 </body>
 
